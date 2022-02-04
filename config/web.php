2d2327b702d31030b5d87b4b49f92c5c37821df3
IvanSukharukov <32388539+IvanSukharukov@users.noreply.github.com>
2022-02-04 19:25:42 +0300

    undo changes basic version

--- config/web.php
+++ config/web.php
@@ -11,7 +11,6 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    // 'defaultRoute' => 'test/index',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -40,7 +40,6 @@ AppAsset::register($this);
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
-            ['label' => 'Test', 'url' => ['/test/index']],  
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
