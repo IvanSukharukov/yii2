d93df9876c57a958369cfe48478d6618002abcd2
IvanSukharukov <32388539+IvanSukharukov@users.noreply.github.com>
2022-02-08 13:27:52 +0300

    form validation, fields, ActiveForm settings

--- models/EntryForm.php
+++ models/EntryForm.php
@@ -9,4 +9,22 @@ class EntryForm extends Model
    public $name;
    public $email;
    public $text;
+
+   public function rules()
+   {
+      return [
+         [['name', 'email', 'text'], 'required'],
+         ['name', 'string', 'min' => 3, 'max' => 12],
+         ['email', 'email'],
+      ];
+   }
+
+   public function attributeLabels()
+   {
+      return [
+         'name' => 'your name',
+         'email' => 'your e-mail',
+         'text' => 'your text',
+      ];
+   }
 }
--- views/test/form.php
+++ views/test/form.php
@@ -1,20 +1,39 @@
 <?php
 
-use yii\widgets\ActiveForm;
+use yii\bootstrap4\ActiveForm;
 use yii\helpers\Html;
 
 ?>
 <div class="col-md-12">
    <h2>Страница с формой</h2>
 
-   <?php $form = ActiveForm::begin() ?>
-   <?= $form->field($model, 'name') ?>
-   <?= $form->field($model, 'email')->radio() ?>
-   <?= $form->field($model, 'text')->textarea(['rows' => 5]) ?>
+   <?php $form = ActiveForm::begin([
+      'id' => 'my-form',
+      'options' => [
+         'class' => 'form-horizontal',
+      ],
+      'fieldConfig' => [
+         'template' => "{label} \n <div class='col-md-5'>{input} \n {hint} \n {error}</div>",
+         'labelOptions' => ['class' => 'col-md-2 control-label'],
+      ]
+   ]) ?>
+
+   <?= $form->field($model, 'name', [
+      'inputOptions' => [
+         'placeholder' => 'test placeholder',
+      ],
+   ])->hint('Enter Your Name') ?>
+   <?= $form->field($model, 'email')->input('email', ['placeholder' => "Enter Your Email"]) ?>
+   <?= $form->field($model, 'text', [
+      'template' => "<div class='col-md-5'>{input} \n {hint} \n {error}</div>",
+   ])->textarea(['rows' => 5]) ?>
 
    <div class="form-group">
-      <?= Html::submitButton('submit', ['class' => 'btn btn-success']) ?>
+      <div class='col-md-5'>
+         <?= Html::submitButton('submit', ['class' => 'btn btn-success']) ?>
+      </div>
    </div>
+
 
    <?php ActiveForm::end() ?>
 </div>
